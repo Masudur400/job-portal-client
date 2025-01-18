352046eeabc0f086dceec9b2a85dcee7b1e14982
Masudur400 <md8004210@gmail.com>
2025-01-18 18:56:04 +0600

    login page complete

--- src/components/Login&Register/Login.jsx
+++ src/components/Login&Register/Login.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        const form = new FormData(e.currentTarget)
+        const email = form.get('email')
+        const password = form.get('password')
+        console.log(email, password);
+    }
+
     return (
         <div className="lg:w-1/3 md:w-1/2 mx-auto my-20 md:p-5 p-3 rounded-lg border border-x-4 border-green-500 shadow-md max-sm:mx-4 ">
-            <h3 className="text-3xl font-medium text-center my-2">Please Login</h3>
-            <form>
+            <h3 className="md:text-3xl text-xl font-medium text-center my-2">Please Login</h3>
+            <form onSubmit={handleLogin}>
                 <div>
-                    <p className='text-medium my-2'>Email*</p>
-                    <input type="email" name="" id="" className='border-2 px-3 py-1 w-full' />
+                    <p className='max-sm-text-sm text-medium my-2'>Email*</p>
+                    <input type="email" name="email" id="" placeholder='Enter Email' className='border-2 px-3 md:py-1 w-full' />
                 </div>
                 <div>
-                    <p className='text-medium my-2'>Password*</p>
-                    <input type="password" name="" id="" className='border-2 px-3 py-1 w-full' />
+                    <p className='max-sm-text-sm text-medium my-2'>Password*</p>
+                    <input type="password" name="password" id="" placeholder='Enter Password' className='border-2 px-3 md:py-1 w-full' />
                 </div>
+                <input type="submit" value="Login" className='px-3 py-1 text-white font-medium bg-green-500 w-full my-4 hover:bg-green-600' />
             </form>
+            <p className='my-3'>Don't have an account <Link to='/loginRegister/register' className='font-medium text-red-500'>please register</Link></p>
         </div>
     );
 };
--- src/components/Login&Register/Register.jsx
+++ src/components/Login&Register/Register.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            Register
+        <div className="lg:w-1/3 md:w-1/2 mx-auto my-20 md:p-5 p-3 rounded-lg border border-x-4 border-green-500 shadow-md max-sm:mx-4 ">
+            <h3 className="md:text-3xl text-xl font-medium text-center my-2">Please Register</h3>
+            <form onSubmit={''}>
+                <div>
+                    <p className='max-sm-text-sm text-medium my-2'>Email*</p>
+                    <input type="email" name="email" id="" placeholder='Enter Email' className='border-2 px-3 md:py-1 w-full' />
+                </div>
+                <div>
+                    <p className='max-sm-text-sm text-medium my-2'>Password*</p>
+                    <input type="password" name="password" id="" placeholder='Enter Password' className='border-2 px-3 md:py-1 w-full' />
+                </div>
+                <input type="submit" value="Login" className='px-3 py-1 text-white font-medium bg-green-500 w-full my-4 hover:bg-green-600' />
+            </form>
+            <p className='my-3'>Already have an account <Link to='/loginRegister/login' className='font-medium text-red-500'>please login</Link></p>
         </div>
     );
 };
