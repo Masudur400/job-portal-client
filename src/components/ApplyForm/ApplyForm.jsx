eb592a5e2032526e642c71e2eb7eb89e82efffc9
Masudur400 <md8004210@gmail.com>
2025-02-19 12:06:08 +0600

    apply send database done

--- src/components/ApplyForm/ApplyForm.jsx
+++ src/components/ApplyForm/ApplyForm.jsx
@@ -2,23 +2,61 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import useAxiosPublic from '../Hooks/useAxiosPublic';
 import useAxiosSecure from '../Hooks/useAxiosSecure';
+import useAuth from '../Hooks/useAuth';
+import toast, { Toaster } from 'react-hot-toast';
 
 const ApplyForm = () => {
 
     const { id } = useParams()
+    const { user } = useAuth()
     const axiosPublic = useAxiosPublic()
     const axiosSecure = useAxiosSecure()
 
+
+    const handleApply = async (e) => {
+        e.preventDefault()
+        const form = new FormData(e.currentTarget)
+        const name = form.get('name')
+        const email = form.get('email')
+        const phone = form.get('phone')
+        const resumeLink = form.get('resumeLink')
+        const userEmail = user?.email
+        const date = new Date()
+
+        const data = {
+            name,
+            email,
+            phone,
+            resumeLink,
+            userEmail,
+            appliedDate: date
+        }
+
+        axiosSecure.post('/applies', data)
+            .then(res =>{
+                if(res.data.insertedId){
+                    toast.success('applied successfully!', {
+                        duration: 1000,
+                        position: 'top-center',
+                    })
+                    e.target.reset()
+                }
+            })
+
+    }
+
+
     return (
         <div className="md:w-1/2 mx-auto my-20 md:p-5 p-3 rounded-lg border border-x-4 border-green-500 shadow-md max-sm:mx-4">
-            <form>
+            <Toaster></Toaster>
+            <form onSubmit={handleApply}>
                 <div>
                     <p className='max-sm-text-sm text-medium my-2'>Name<span className='text-red-500'>*</span></p>
                     <input required type="text" name="name" id="" placeholder='Enter your name' className='border-2 px-3 md:py-1 w-full' />
                 </div>
                 <div>
                     <p className='max-sm-text-sm text-medium my-2'>Email<span className='text-red-500'>*</span></p>
-                    <input required type="text" name="email" id="" placeholder='Enter your email' className='border-2 px-3 md:py-1 w-full' />
+                    <input required type="text" defaultValue={user?.email} name="email" id="" placeholder='Enter your email' className='border-2 px-3 md:py-1 w-full' />
                 </div>
                 <div>
                     <p className='max-sm-text-sm text-medium my-2'>Phone<span className='text-red-500'>*</span></p>
@@ -28,6 +66,7 @@ const ApplyForm = () => {
                     <p className='max-sm-text-sm text-medium my-2'>Resume Link<span className='text-red-500'>*</span></p>
                     <input required type="text" name="resumeLink" id="" placeholder='Enter your resume link' className='border-2 px-3 md:py-1 w-full' />
                 </div>
+                <input type="submit" value="Submit" className='px-3 py-1 text-white font-medium bg-green-500 w-full my-4 hover:bg-green-600' />
             </form>
         </div>
     );
