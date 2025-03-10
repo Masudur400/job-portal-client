0ef6766bdd5d457a731e4351650c68b45547b8db
Masudur400 <md8004210@gmail.com>
2025-03-10 18:57:19 +0600

    axios hooks call

--- src/components/Hooks/useModerator.jsx
+++ src/components/Hooks/useModerator.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import useAxiosSecure from './useAxiosSecure';
+import useAxiosPublic from './useAxiosPublic';
 
 const useModerator = () => {
+
+    const axiosSecure = useAxiosSecure()
+    const axiosPublic = useAxiosPublic()
+
     return (
         <div>
             use moderator
