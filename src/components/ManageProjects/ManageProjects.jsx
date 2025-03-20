6b9c89eb9e8cd0faf43078d4b5b2715d63282df1
Masudur400 <md8004210@gmail.com>
2025-03-20 19:21:01 +0600

    query projects from server

--- src/components/ManageProjects/ManageProjects.jsx
+++ src/components/ManageProjects/ManageProjects.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import useAxiosSecure from '../Hooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
 
 const ManageProjects = () => {
+
+    const axiosSecure = useAxiosSecure();
+
+
+    const { data: allUser = [], isLoading, refetch } = useQuery({
+        queryKey: ['projects', axiosSecure],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/projects');
+            return res.data;
+        }
+    });
+
     return (
         <div>
             Manage Projects
