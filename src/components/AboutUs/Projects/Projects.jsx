0f1b4a45ddcda3fdac54a68055ed24b543d8f19b
Masudur400 <md8004210@gmail.com>
2025-03-25 19:16:05 +0600

    project show about page

--- src/components/AboutUs/Projects/Projects.jsx
+++ src/components/AboutUs/Projects/Projects.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import useAxiosPublic from '../../Hooks/useAxiosPublic';
+import useAxiosSecure from '../../Hooks/useAxiosSecure';
+import SingleProject from './SingleProject';
+import { useQuery } from '@tanstack/react-query';
 
 const Projects = () => {
+
+    const axiosSecure = useAxiosSecure()
+    const axiosPublic = useAxiosPublic()
+
+    const { data: projects = [], isLoading, refetch } = useQuery({
+        queryKey: ['projects', axiosPublic],
+        queryFn: async () => {
+            const res = await axiosPublic.get(`/projects`);
+            return res.data;
+        }
+    });
+
     return (
         <div>
             <h2 className="text-3xl font-bold text-center my-10">Projects</h2>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            {
+                projects.map (project => <SingleProject project={project} key={project._id}></SingleProject>)
+            }
+        </div>
         </div>
     );
 };
--- src/components/AboutUs/Projects/SingleProject.jsx
+++ src/components/AboutUs/Projects/SingleProject.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const SingleProject = () => {
+const SingleProject = ({project}) => {
+    
+    const {projectName,technologies,projectCover,projectURL,date} = project
+
     return (
-        <div>
-            
+        <div className='space-y-3 shadow-md p-3 rounded-sm group'>
+            <img src={projectCover} alt="img" className='group-hover:scale-105' />
+            <p className='text-lg font-medium'>{projectName}</p>
+            <p className='text-sm'>{technologies}</p>
+            <Link to={projectURL} target='blank'><button className='px-3 py-1 my-3 text-white font-medium bg-green-500 w-full rounded-sm hover:bg-green-600'>View Project</button></Link>
         </div>
     );
 };
