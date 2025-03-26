620e3161996bebd6a0cbc4ecb5dcea50e062e573
Masudur400 <md8004210@gmail.com>
2025-03-26 19:13:42 +0600

    admin home page setup

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,37 @@
 import { Helmet } from "react-helmet";
 import About from "./About";
-import Banner from "./Banner"; 
+import Banner from "./Banner";
 import Services from "./Services";
+import useAdmin from "../Hooks/useAdmin";
+import useModerator from "../Hooks/useModerator";
+import Loading from "../Loading/Loading";
+import AdminHome from "./AdminHome";
+import { div } from "framer-motion/client";
 
 const Home = () => {
+
+    const [isAdmin, isAdminLoading] = useAdmin()
+    const [isModerator, isModeratorLoading] = useModerator()
+
+    if (isModeratorLoading || isAdminLoading) {
+        return <Loading></Loading>
+    }
+
     return (
         <div>
             <Helmet>
                 <title>Home</title>
             </Helmet>
-            <Banner></Banner>
-             <About></About>
-             <Services></Services> 
+            {
+                isAdmin || isModerator ? <div>
+                    <AdminHome></AdminHome>
+                </div> :
+                    <div>
+                        <Banner></Banner>
+                        <About></About>
+                        <Services></Services>
+                    </div>
+            }
         </div>
     );
 };
